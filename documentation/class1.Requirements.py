2e9a678faf7f7abc655b94df63f3da7f43184ae7
Luis Ricardo Guerra Arroyo <luiskrieg@gmail.com>
2021-05-12 19:34:51 -0500

    Setup backend service and draft requirements definition

---
+++ documentation/class1.Requirements.py
@@ -0,0 +1,18 @@
+project_technologies = "Python + Flask + Bootstrap 4 / VueJS"
+project_description = f"Create a Web Business app with {project_technologies}"
+
+print(project_description)
+
+project_deliverables_one = "Analysis and design phases (project documentation)"
+project_deliverables_two = "Business app estructure (flask + MySQL)"
+project_deliverables_three = "VueJS"
+
+project_deliverables_date_one = "Wednesday June 4th, 2021"
+project_deliverables_date_two = "Wednesday July 7th, 2021"
+project_deliverables_date_three = "Wednesday August 4th, 2021"
+
+project_objectives = "What is your business application about"
+
+
+for_backend = "Flask=1.1.2"
+for_frontend = "node, npm, vue, vuecli"
