3b26d196a6ff5fd98369f6816514fc886756151a
Luis Ricardo Guerra Arroyo <luiskrieg@gmail.com>
2021-05-12 19:59:19 -0500

    Setup frontend with VueJS

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'Main',
+  data () {
+    return {
+      message: "Learning VUE JS 2021, with Luis Guerra & Juanito"
+    }
+  }
+}
+
+</script>
+
